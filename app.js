848818f7d396521788b6ea08981bff65a0b6e803
SamaraMohsin2211014KHICND <samara.mohsin@xloopdigital.com>
2022-12-02 19:08:52 +0500

    refactor in code

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-const Book = require('./Book');
-const ReadingList = require('./ReadingList');
 
-let myReadingList = new ReadingList();
-//Arrange
-let book1 = new Book("The Snow Crash","Neal Strephson",200,1992);
-let book2 = new Book("The Earth","Unknown1",300,2000);
-let book3 = new Book("The Sun","Unknown2",500,1999);
-// Act
-myReadingList.addBook(book1,'Dec 02,2022',4);
-myReadingList.addBook(book2,'Dec 05,2022',3);
-myReadingList.addBook(book3,'Dec 07,2022',4);
-let ReadBooks = myReadingList.getBooksByRating(4);
-console.log(ReadBooks);
 
 
 
